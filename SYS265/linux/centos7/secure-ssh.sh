10b7d8fd8554e6af5aa9358905354e00e46f4be7
root <root@web01-michael.michael.local>
2024-02-27 19:45:49 -0500

    added this in

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author gmcyber
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled form the remote repo
+#rmeotes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
