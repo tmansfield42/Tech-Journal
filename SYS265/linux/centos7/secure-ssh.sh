894e4859bc6887241a5350794502cadea869d131
tmansfield42 <michael.mansfield@mymail.champlain.edu>
2024-02-28 01:33:54 +0000

    passwordless login user creation script

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,11 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled form the remote repo
 #rmeotes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo -n "Enter user: "
+read user
+useradd -m -d /home/$user -s /bin/bash $user
+mkdir /home/$user/.ssh
+cp SYS265/linux/public-keys/id_rsa.pub /home/$user/.ssh/authorized_keys
+chmod 700 /home/$user/.ssh
+chmod 600 /home/$user/.ssh/authorized_keys
+chown -R $user:$user /home/$user/.ssh
