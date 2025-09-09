bda57f73778b7a46c506e67c22c403ae644f49a7
tmansfield42 <113706479+tmansfield42@users.noreply.github.com>
2025-09-09 16:04:21 -0400

    Create portscanner.sh

---
+++ portscanner.sh
@@ -0,0 +1,9 @@
+hostfile=$1
+portfile=$2
+echo "host,port"
+for host in $(cat $hostfile); do
+for host in $(cat $portfile); do
+  timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
+    echo "$host,$port"
+    done
+  done
