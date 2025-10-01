9454292e165c9723b400cad07937eb41d9e22b95
Michael <michael.mansfield@mymail.champlain.edu>
2025-10-01 01:42:53 -0400

    Scraping Champlain Classes

---
+++ sys-320/week05/Classes.ps1
@@ -0,0 +1,39 @@
+﻿function classes{
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.16/Courses2025FA.html
+$trs = $page.ParsedHtml.body.getElementsByTagName("tr")
+$table = @()
+    for($i=1; $i -lt $trs.length; $i++){
+    $tds = $trs[$i].getElementsByTagName("td")
+    $times = $tds[5].innerText.Split("-")
+  
+    $table += [PSCustomObject]@{"Class Code" = $tds[0].innerText;
+                                "Title" = $tds[1].innerText;
+                                "Days"  = $tds[4].innerText
+                                "Time_start" = $Times[0];
+                                "Time_end" = $Times[1]
+                                "Instructor" = $tds[6].innerText;
+                                "Location" = $tds[9].innerText;
+                                }
+
+}
+$table = translate($table)
+return $table
+}
+
+function translate($table){
+
+
+for($i=0; $i -lt $table.length; $i++){
+    $days = @()
+
+    if($table[$i].Days -ilike "M*"){  $days += "Monday"  }
+    if($table[$i].Days -ilike "*T[*F]*"){  $days += "Tuesday"  }
+    if($table[$i].Days -ilike "T"){  $days += "Tuesday"  }
+    if($table[$i].Days -ilike "W"){  $days += "Wednesday"  }
+    if($table[$i].Days -ilike "*TH"){  $days += "Thursday"  }
+    if($table[$i].Days -ilike "F*"){  $days += "Friday"  }
+
+$table[$i].Days = $days
+}
+return $table
+}
---
+++ sys-320/week05/InstructorClassCount.ps1
@@ -0,0 +1,21 @@
+﻿. "C:\Users\Michael\Desktop\Classes.ps1"
+
+$table = classes
+
+$mytable = $table | Where-Object { ($_."Class Code" -ilike "SYS*") -or 
+                        ($_."Class Code" -ilike "NET*") -or 
+                        ($_."Class Code" -ilike "SEC*") -or
+                        ($_."Class Code" -ilike "FOR*") -or
+                        ($_."Class Code" -ilike "CSI*") -or
+                        ($_."Class Code" -ilike "DAT*")
+ } #| Select-Object "Instructor" | Sort-Object "Instructor" -Unique
+
+
+ 
+ $table | where { ($_.Instructor -in $mytable.Instructor)  -and ($_.Instructor -notlike "*8/25**")} | Group-Object "Instructor" 
+ | Sort-Object Count, Name | Sort-Object Count -Descending | Format-Table -Property Count, Name
+
+
+# Select-Object "Class Code", Instructor, Location, Days, "Time_start", "Time_end"
+
+ 
