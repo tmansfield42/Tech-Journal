ffdca27839cc3661535ee8c1e41c837e8a21aad9
tmansfield42 <113706479+tmansfield42@users.noreply.github.com>
2026-04-20 21:31:22 -0400

    milestone 9 work

--- SEC-480/modules/480-utils/480-utils.psm1
+++ SEC-480/modules/480-utils/480-utils.psm1
@@ -207,18 +207,12 @@ Function Set-Networkk(){
         Set-NetworkAdapter -NetworkAdapter $adapter -NetworkName $net 
         Write-Host "updated $($adapter.Name) on $($macname.Name) to $net"
 
-
-
-
-    
 }
- 
+
 Function Set-WindowsIP(){
-    # Connect to vCenter and load config
     $conf = Get-480Config -config_path "/home/michael/Tech-Journal/SEC-480/480.json"
     480Connect -server $conf.vcenter_server
- 
-    # Pick the VM to configure
+
     $vms = Get-VM
     $index = 1
     foreach($i in $vms)
@@ -229,31 +223,22 @@ Function Set-WindowsIP(){
     $pick = Read-Host "Which VM do you wish to set a static IP on?"
     $vm = $vms[$pick -1]
     Write-Host "You picked " $vm.Name
- 
-    # Collect network info
-    $ip = Read-Host "Enter the static IP address "
-    $netmask = Read-Host "Enter the subnet mask "
-    $gateway = Read-Host "Enter the default gateway"
-    $dns = Read-Host "Enter the DNS server"
-    $interface = Read-Host "Enter the interface name"
- 
-    # Collect guest credentials securely
+
+    $ip = Read-Host "Enter the static IP address (e.g. 10.0.5.5)"
+    $netmask = Read-Host "Enter the subnet mask (e.g. 255.255.255.0)"
+    $gateway = Read-Host "Enter the default gateway (e.g. 10.0.5.2)"
+    $dns = Read-Host "Enter the DNS server (e.g. 10.0.5.5)"
+    $interface = Read-Host "Enter the interface name (e.g. Ethernet0)"
+
     $guestUser = Read-Host "Enter the guest username (e.g. deployer)"
     $securePass = Read-Host "Enter the guest password" -AsSecureString
     $guestPassword = (New-Object System.Net.NetworkCredential("", $securePass)).Password
- 
-    # Build netsh commands
-    $script = @"
-netsh interface ip set address name="$interface" static $ip $netmask $gateway
-netsh interface ip set dns name="$interface" static $dns
-"@
- 
+
+    $line1 = "netsh interface ip set address name=`"$interface`" static $ip $netmask $gateway"
+    $line2 = "netsh interface ip set dns name=`"$interface`" static $dns"
+    $script = "$line1`r`n$line2"
+
     Write-Host "Setting static IP on $($vm.Name)..." -ForegroundColor Yellow
     Invoke-VMScript -VM $vm -ScriptText $script -GuestUser $guestUser -GuestPassword $guestPassword -ScriptType Bat
     Write-Host "Static IP set to $ip on $($vm.Name)" -ForegroundColor Green
-}
- 
-        
-
-
 }
